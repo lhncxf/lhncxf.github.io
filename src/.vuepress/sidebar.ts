16b674c6a68d6d01e26d61b8ba35332ea8f25d69
Kobe.Lu <kobeluhaonan@gmail.com>
2025-02-19 10:49:49 +0800

    update

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,4 +1,9 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
+    "/docs/program/": 'structure',
+    "/docs/posts/": 'structure',
+    "/docs/reading/": 'structure',
+    "/docs/diary/": 'structure',
+    "/docs/others/": 'structure'
 });
